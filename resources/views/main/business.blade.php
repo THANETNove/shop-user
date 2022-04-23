d15061770c49e791dbafd69eefda34d1c63f572e
THANETNove <Novel_Manga@outlook.co.th>
2022-04-24 05:00:23 +0700

    อัพใหม่

--- resources/views/main/business.blade.php
+++ resources/views/main/business.blade.php
@@ -18,73 +18,56 @@
              <br>
              <p>ชิ้นคู่</p>
             </div>
+
         </div>
     </div>
 </div>
 <div class="border-th ">
-    <p  class="font-size span">[202204230462] กำหนดเวลาการจับคู่คำสั่งชื้อ<span>00:00:13</span></p>
-</div>
-<div class=py-12>
-    <div class="container">
-        <div class="row head-center text-offcanvas down12 ">
-            <div class="col-3 col-sm-3 col-md-3">
-                <button type="button" class="btn btn-secondary">ชิ้นใหญ่</button>
-            </div>
-            <div class="col-3 col-sm-3 col-md-3">
-                <button type="button" class="btn btn-secondary">ชิ้นเล็ก</button>
-            </div>
-            <div class="col-3 col-sm-3 col-md-3">
-                <button type="button" class="btn btn-secondary">ชิ้นเดี่ยว</button>
-            </div>
-            <div class="col-3 col-sm-3 col-md-3">
-                <button type="button" class="btn btn-secondary">ชิ้นคู่</button>
-            </div>
-        </div>
-    </div>
+    <p  class="font-size span">[202204230462] กำหนดเวลาการจับคู่คำสั่งชื้อ<span id="countingdown"></span></p>
 </div>
 <table class="table table-bordered down12">
     <tbody>
       <tr class="img-shop box-shop center">
-        <td class="col-3 col-sm-3 col-md-4 .box-shop-padding " >
-            <img src="{{asset('/image/shop-21.png')}}" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom" class="img-shop-tb-2" alt="...">
+        <td class="col-3 col-sm-3 col-md-4 .box-shop-padding nameShop"  data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom"  >
+            <img src="{{asset('/image/shop-21.png')}}" class="img-shop-tb-2" alt="...">
            <br>
             <span class="center-shop">SHOPEE</span>
         </td>
-        <td class="col-3 col-sm-3 col-md-4" >
+        <td class="col-3 col-sm-3 col-md-4 nameShop" >
             <img src="{{asset('/image/shop-22.png')}}" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom" class="img-shop-tb-2" alt="...">
             <br>
             <span class="center-shop">LAZADA</span>
         </td>
-            <td class="col-3 col-sm-3 col-md-4" >
+            <td class="col-3 col-sm-3 col-md-4 nameShop" >
                 <img src="{{asset('/image/shop-23.png')}}" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom" class="img-shop-tb-2" alt="...">
               <br>
                 <span class="center-shop">SHOP24</span>
             </td>
       </tr>
       <tr class="img-shop box-shop center">
-        <td class="col-3 col-sm-3 col-md-4 " >
+        <td class="col-3 col-sm-3 col-md-4 nameShop" >
             <img src="{{asset('/image/shop-24.png')}}" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom" class="img-shop-tb-2" alt="...">
             <br>
             <span class="center-shop">JIB</span>
         </td>
-        <td class="col-3 col-sm-3 col-md-4" >
+        <td class="col-3 col-sm-3 col-md-4 nameShop" >
             <img src="{{asset('/image/shop-25.png')}}" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom" class="img-shop-tb-2" alt="...">
            <br>
             <span class="center-shop">JD CENTRAL</span>
         </td>
-            <td class="col-3 col-sm-3 col-md-4" >
+            <td class="col-3 col-sm-3 col-md-4 nameShop" >
                 <img src="{{asset('/image/shop-26.png')}}" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom" class="img-shop-tb-2" alt="...">
                 <br>
                 <span class="center-shop">Chilindo</span>
             </td>
       </tr>
-      <tr class="img-shop box-shop center">
+      <tr class="img-shop box-shop center nameShop">
         <td class="col-3 col-sm-3 col-md-4 " >
             <img src="{{asset('/image/shop-27.png')}}" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom" class="img-shop-tb-2" alt="...">
             <br>
             <span class="center-shop">CENTRAL</span>
         </td>
-        <td class="col-3 col-sm-3 col-md-4" >
+        <td class="col-3 col-sm-3 col-md-4 nameShop" >
             <img src="{{asset('/image/shop-28.png')}}" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom" class="img-shop-tb-2" alt="...">
             <br>
             <span class="center-shop">POWER BUY</span>
@@ -95,34 +78,48 @@
   <br>
   <br>
   <br>  
+  @php
+      $money =  number_format(Auth::user()->money,2)
+     @endphp
+     <p  id="money-user" style="display:none;"> {{ $money}}</p>
   <div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
     <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasBottomLabel"></h5>
+      <h5 class="offcanvas-title font-size">จำนวนเงินคงเหลือ  {{ $money}} ฿</h5>
       <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body small">
+        <p class="offcanvas-title font-size" id="error-price"></p>
         <div class=py-12>
             <div class="container">
-                <div class="row head-center text-offcanvas down12 ">
-                    <div class="col-4 col-sm-4 col-md-4">
-                        <select class="form-select" aria-label="Default select example">
-                            <option selected>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                <div class="row text-offcanvas down12 ">
+                    <div class="mb-3">
+                        <p for="exampleFormControlInput1" class="form-label font-size-14">ชื่อสินค้า</p>
+                        <input type="text" class="form-control" id="nameshop-1" name="nameshop" >
+                      </div>
+                      <div class="mb-3">
+                        <p for="exampleFormControlInput1"  class="form-label font-size-14">ขนาดสินค้า</p>
+                        <select class="form-select"  id="size"  name="size" aria-label="Default select example">
+                            <option  value="ชิ้นใหญ่" selected>ชิ้นใหญ่</option>
+                            <option value="ชิ้นเล็ก">ชิ้นเล็ก</option>
+                            <option value="ชิ้นคู่">ชิ้นคู่</option>
+                            <option value="ชิ้นเดียว">ชิ้นเดียว</option>
                           </select>
-                    </div>
-                    <div class="col-4 col-sm-4 col-md-4">
-                        <select class="form-select" aria-label="Default select example">
-                            <option selected>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                      </div>
+                      <div class="mb-3">
+                        <p for="exampleFormControlInput1"    class="form-label font-size-14">ขนาดสินค้า</p>
+                        <select class="form-select" id="price" name="price" aria-label="Default select example">
+                            <option value="25" selected>25</option>
+                            <option value="100">100</option>
+                            <option value="5">500</option>
+                            <option value="1 k">1 k</option>
+                            <option value="2.5 k">2.5 k</option>
+                            <option value="5 k">5 k</option>
                           </select>
+                      </div>
+                    <div class="mb-3  head-center">
+                        <button type="button" id="buy-shop" class="btn btn-outline-light">ซื้อสินค้า</button>
                     </div>
-                    <div class="col-4 col-sm-4 col-md-4">
-                        <button type="button" class="btn btn-danger">ซื้อสินค้า</button>
-                    </div>
+                    <br>
                 </div>
             </div>
         </div>     
