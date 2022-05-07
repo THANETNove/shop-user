dd96856462bf7314775541270311bd5f8d35e1a9
rattanakorn31128 <rattanakxxn@gmail.com>
2022-05-07 14:29:49 +0700

    UPCODE

--- resources/views/layouts/navbarFooter.blade.php
+++ resources/views/layouts/navbarFooter.blade.php
@@ -1,28 +1,35 @@
 <div class="footer">
     <div class="container">
         <div class="row row-cols-auto">
-          <div class="col-3 col-sm-3 col-md-3">
+          <div class="col-3 col-sm-3 col-md-2">
                 <i class="fa-solid fa-house-chimney active navbarFooter" id="home" value="null" style='font-size:28px'>
                 <br>
-              <span class="icon-text">หน้าหลัก</span>
+              <span class="icon-text">บ้าน</span>
               </i>
             </div>
-          <div class="col-3 col-sm-3 col-md-3 ">
+          <div class="col-3 col-sm-3 col-md-3">
               <i class="fa-solid fa-location-dot navbarFooter"  id="location"  value="{{$name}}" style='font-size:28px'>
                 <br>
-                <span class="icon-text">รายชื่อธุรกิจ</span>
+                <span class="icon-text">เติมเงิน</span>
               </i>
             </div>
-          <div class="col-3 col-sm-3  col-md-3"> 
+            <div class="col-3 col-sm-3 col-md-2">
+              <div class="icon-center">
+              <i class="fa-duotone fa-360-degrees"></i>
+              <i class="fa-thin fa-circle-bolt"></i>
+            <!--   <span class="text-color">คว้า</span> -->
+              </div>
+            </div>
+          <div class="col-3 col-sm-3  col-md-2"> 
               <i class="fa-solid fa-gift navbarFooter" id="gift"  value="{{$name}}" style='font-size:28px'>
                 <br>
-                <span class="icon-text">ของขวัญ</span>
+                <span class="icon-text">คำสั่ง</span>
               </i>
             </div>
-            <div class="col-3 col-sm-3 col-md-3"> 
+            <div class="col-3 col-sm-3 col-md-2"> 
                 <i class="fa-solid fa-user navbarFooter" id="user"  value="{{$name}}" style='font-size:28px'>
                   <br>
-                  <span class="icon-text">ข้อมูลของฉัน</span>
+                  <span class="icon-text">บัญชี</span>
                 </i>
             </div>
         </div>
