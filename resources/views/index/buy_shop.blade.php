87950f2722ac2e23d6cc747767b0a435e83996dc
THANETNove <novel_manga@outlook.co.th>
2022-05-08 15:19:32 +0700

    asdas

---
+++ resources/views/index/buy_shop.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.home')
+@section('content')
+
+<div class="head logo-center buy_shop">
+    <div class="set-head ">
+        <p class="text">รายละเอียดการสั่งชื้อ</p>
+    </div>
+
+
+</div>
+
+
+
+@include('layouts.navbarFooter2')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -120,7 +120,7 @@ Route::post('/buy', [App\Http\Controllers\BuyOutController::class ,'store'] );
 Route::get('/reserve', [App\Http\Controllers\BuyOutController::class ,'index'] );
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/topping/{name}', [App\Http\Controllers\ToppingController::class, 'index']);
-Route::get('/shop_index', [App\Http\Controllers\IndexController::class, 'index']);
+Route::resource('/shop_index', App\Http\Controllers\IndexController::class,);
 Route::get('/topUp', [App\Http\Controllers\IndexController::class, 'topUp']);
 });
 /* admin */
