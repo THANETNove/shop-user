2d9b8e57289671933e8d9f420a32c457f0b55259
thanet <thanet@planforfit.com>
2022-05-08 11:02:26 +0700

    upcode

--- resources/views/index/shop_index.blade.php
+++ resources/views/index/shop_index.blade.php
@@ -1,33 +1,28 @@
 @extends('layouts.home')
 @section('content')
-<div class="head logo-center active1">
-{{--     <div class="set-back">
+    <div class="head logo-center active1">
+        {{-- <div class="set-back">
         <a href="{{ URL::to('shop')}}">
             <i class="fa-solid fa-arrow-left" style='font-size:28px'></i>
         </a>
-    </div>
-    --}}
-    <div class="set-head ">
-        <p  class="text">บันทึก</p>
-    </div>
-    <div class="container">
-        <div class="row">
-          <div class="col-12">
-            <button type="button" class="btn btn-light bottom1">รอดำเนินการ</button>
-          </div>
+    </div> --}}
+        <div class="set-head ">
+            <p class="text">บันทึก</p>
         </div>
-      </div>
-      
-        <div class="row">
-            <div class="box1">
-               <div align="left">
-                   <span>วันเดือนปี</span>
-               </div>
-               <div align="left">
-                    <img class="imgas" src="{{asset('/image/A-1.png')}}">
-               </div>
+        <div class="col-12 col-md-12 col=lg-12">
+            <button type="button" class="btn btn-light bottom1">รอดำเนินการ</button>
+        </div>
 
-                
+        <div class="row">
+            <div class="box1 col-10">
+                <div align="left">
+                    <span>วันเดือนปี</span>
+                </div>
+                <div align="left">
+                    <img class="imgas" src="{{ asset('/image/A-1.png') }}">
+                </div>
+
+
             </div>
         </div>
 
@@ -35,11 +30,11 @@
 
 
 
-</div>
+    </div>
 
 
 
 
 
-@include('layouts.navbarFooter2')
+    @include('layouts.navbarFooter2')
 @endsection
--- resources/views/top_up/topUp.blade.php
+++ resources/views/top_up/topUp.blade.php
@@ -1,1 +1,14 @@
-asdasd
+@extends('layouts.home')
+@section('content')
+<div class="head logo-center active1">
+    <div class="set-head ">
+        <p  class="text">บันทึก</p>
+    </div>
+</div>
+
+
+
+
+
+@include('layouts.navbarFooter2')
+@endsection
