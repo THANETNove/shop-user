904cd1b71e149b382048aa7a218beef6c49dc220
THANETNove <novel_manga@outlook.co.th>
2022-05-10 01:59:41 +0700

    แก้ไขบัญชี

--- resources/views/index/up_Image.blade.php
+++ resources/views/index/up_Image.blade.php
@@ -1,40 +1,32 @@
 @extends('layouts.home')
 @section('content')
-<div class="col-12 col-sm-12 col-md-12 col-lg-12 ">
-
-    <div class="box-3">
-        <div class="container">
-            <div class="row">
-                <div class=" col-12 col-sm-12 col-md-4 col-lg-4">
-                    <div class="box-4">
-                        <img src="{{asset('/image/A-2.png')}}" class="money5 ">
-                    </div>
-                </div>
-                <div class=" col-12 col-sm-12 col-md-8 col-lg-8">
-                    <div class="row">
-                        <div class="col-6  col-lg-6">
-                            <p>ชื่อธนาคาร</p>
+    <div class="container">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12 ">
+            <div class="box-3">
+                <div class="container">
+                    <div class="row row-cols-2 bank2">
+                        <div class="col-6 col-lg-6 ">
+                            <p class="bank3">ชื่อบัญชีธนาคาร</p>
                         </div>
-                        <div class="col-6  col-lg-6">
-                            <p>เกียรตินาคินภัทร</p>
+                        <div class="col-6 col-lg-6 ">
+                            <P class="bank3">เกียรตินาคินภัทร</P>
                         </div>
-                        <div class="col-6  col-lg-6">
-                            <p>ชื่อบัญชี</p>
+                        <div class="col-6 col-lg-6 ">
+                            <p class="bank3">ชื่อบัญชี</p>
                         </div>
-                        <div class="col-6  col-lg-6">
-                            <p>น.ส. ศิสพร ฉิมสวัสดิ์</p>
+                        <div class="col-6 col-lg-6 ">
+                            <P class="bank3">ศิวพร ฉิมสวัสดิ์</P>
                         </div>
-                        <div class="col-6  col-lg-6">
-                            <p>เลขบัญชี</p>
+                        <div class="col-6 col-lg-6 ">
+                            <p class="bank3">เลขบัญชี</p>
                         </div>
-                        <div class="col-6  col-lg-6">
-                            <p>200-618080-5</p>
+                        <div class="col-6 col-lg-6 ">
+                            <P class="bank3">200-618080-5</P>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div>    
     </div>
+    @endsection
     {{$idAmounts}}
-</div>
-@endsection
